9e8e9b89075a763fc3431897b6bc44c447779e76
John Doe <johndoe@example.com>
2022-06-14 15:35:07 +0800

    ccommit 26 : add flash allert

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,11 +10,13 @@ class UsersController < ApplicationController
   def create 
     @user = User.new(user_params)
     if @user.save
-      ################
+      flash[:success] = "Welcome to the Twitter Clone App"
+      redirect_to @user
     else
       render 'new'
     end
   end
+  
   def index
 
   end
